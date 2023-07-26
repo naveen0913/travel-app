8e404451fc1eaa1a781aeb30cd3bc82783186cad
U Naveen Babu <136116172+naveen0913@users.noreply.github.com>
2023-07-26 17:17:52 +0530

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,31 +1,5 @@
 import axios from 'axios'
 
-
-/*
-
-const options = {
-  method: 'GET',
-  url: 'https://travel-advisor.p.rapidapi.com/hotels/list-in-boundary',
-  params: {
-    bl_latitude: '11.847676',
-    bl_longitude: '108.473209',
-    tr_longitude: '109.149359',
-    tr_latitude: '12.838442',
-    limit: '30',
-    currency: 'USD',
-    subcategory: 'hotel,bb,specialty',
-    adults: '1'
-  },
-  headers: {
-    'X-RapidAPI-Key': '828c69946cmsh6f7d430082a7da9p138656jsn8b5d99575e2c',
-    'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
-  }
-};
-
-
-
-*/
-
  const getPlacesData=async(type,sw,ne)=>{
     try{
         const {data:{data}}= await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,{
